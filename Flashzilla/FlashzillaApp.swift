f701b4035c14840e886e75c161821c6d9c0af99d
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-07-29 18:17:23 -0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Filipe Fernandes on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
